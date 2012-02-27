64f09c12dd11103868344f5b14895eae61d6cc35
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-02-27 16:29:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdsync::Application.config.secret_token = '51330cca2e31897e340dd91785d76d7dac5fec994ce8fd7c4c5683af3da06e2085741db1b2e783692487aa1a3f49392fca37d2de2ea2ad14d6b110712a0905e5'
