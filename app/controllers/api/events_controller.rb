0063711e16c9a6a25a156840ad01389a8637b8b6
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-02-29 19:48:23 +0200

    Add willpaginate gem. Add constants.rb inializer

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,13 +1,13 @@
 class Api::EventsController < Api::BaseController
      
   def index
-    events = api_hashes_array(Event.all)
+    events = api_hashes_array(Event.paginate(:page => params[:page], :per_page => ITEMS_PER_PAGE))
        
     render :status => "200", :json => {:success => true, :description => "Events list.", :result => events}
   end
   
   def top
-    events = api_hashes_array(Event.all)
+    events = api_hashes_array(Event.paginate(:page => params[:page], :per_page => ITEMS_PER_PAGE))
     
     render :status => "200", :json => {:success => true, :description => "Top Events list.", :result => events}
   end
