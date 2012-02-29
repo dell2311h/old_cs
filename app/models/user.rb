375fbebec0aec732fbdc9aeef75ce0db233df9fd
M Kurtikov <mk@mail.dimalex.eu/>
2012-02-29 14:41:52 +0200

    added validators to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  
+  validates :name, :login, :email, :password, :presence => true
+
+  validates :email, :email => true 
+  
+  validates :login, :password,
+            :length => {:minimum => 5, :maximum => 255}
+  
+  validates :login, :email, :uniqueness => true
+  
+  validates :age, :numericality => { :only_integer => true }
+  
 end
