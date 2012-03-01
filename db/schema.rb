69fa581544fd96936f492d81a9d5b3dccaf03c4b
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-01 12:49:10 +0200

    Fix column name at places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229132926) do
+ActiveRecord::Schema.define(:version => 20120301102307) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(:version => 20120229132926) do
   create_table "places", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.integer  "latiture"
+    t.integer  "latitude"
     t.integer  "longitude"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
