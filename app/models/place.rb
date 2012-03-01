f8a030a269c60df241540e20bc786e4abb8a46b5
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-01 13:23:31 +0200

    Add tests for Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,13 @@
 class Place < ActiveRecord::Base
   has_many :events
   belongs_to :user
+  
+  validates :name, :user_id, :latitude, :longitude, :presence => true
+  
+  def api_data
+    hash = self.attributes
+    hash.delete("created_at")
+    hash.delete("updated_at")
+    hash
+  end  
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Place do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @place = Factory(:place)
+  end
+  
+  subject { @place }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:latitude) }  
+  it { should respond_to(:longitude) }
+  it { should respond_to(:user_id) }  
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:latitude) }
+  it { should validate_presence_of(:longitude) }
+  it { should validate_presence_of(:user_id) }
+  
+  it { should respond_to :api_data }
+  
+  it "should return api data in appropriate format" do
+    @place.api_data.keys.should ==  ["id", "name", "user_id", "latitude", "longitude"]
+  end
+  
 end
