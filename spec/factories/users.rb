912278c0e207d7ca899c68a62999d2792f0333fc
M Kurtikov <mk@mail.dimalex.eu/>
2012-03-01 19:20:41 +0200

    Fix for user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,9 @@ FactoryGirl.define do
   factory :user do
     name     Faker::Name.name
     email    Faker::Internet.email
-    username    Faker::Lorem.words(1)
-    password    Faker::Lorem.words(1)
-    phone    Faker::PhoneNumber
+    username    Faker::Internet.user_name
+    password    "passwordddd"
+    phone    Faker::PhoneNumber.phone_number
     age      20
   end
 end
