029f971cdfeb979d2eb1d206411a851dd6142c20
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-01 19:14:03 +0200

    PlacesController works. Add SEARCH_RADIUS constant

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,9 @@ class Place < ActiveRecord::Base
   
   validates :name, :user_id, :latitude, :longitude, :presence => true
   
-  scope :with_name_like, lambda {|name| where("UPPER(name) LIKE ?", "%#{name.upcase}%") }
+  scope :with_name_like, lambda {|name| where("UPPER(name) LIKE ?", "%#{name.to_s.upcase}%") }
+  
+  reverse_geocoded_by :latitude, :longitude
   
   def api_data
     hash = self.attributes
