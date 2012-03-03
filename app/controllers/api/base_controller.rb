6b2e3015fd23ee735d411127f7d65b0cd41c899e
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-03 19:54:42 +0200

    Move exception handling to BaseController

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,4 +3,10 @@ class Api::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token
   respond_to :json
 
+
+  rescue_from Exception do |exeption|
+    respond_with :error => exeption.message, :status => :bad_request
+  end
+
 end
+
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -7,6 +7,11 @@ class Api::EventsController < Api::BaseController
       events = events.order_by_video_count
     end
 
+    if params[:nearby]
+      raise "Coordinates are not provided" unless params[:latitude] && params[:longitude]
+      events = events.nearby [params[:latitude], params[:longitude]], SEARCH_RADIUS
+    end
+
     if events.count > 0
       events = events.paginate(:page => params[:page], :per_page => ITEMS_PER_PAGE)
       respond_with events, :status => :ok
