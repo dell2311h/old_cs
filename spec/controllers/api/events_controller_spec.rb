2fbabb51b0fd0aa7ebba99e83977ef3db77ed086
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-05 00:56:24 +0200

    Refactor tests for EventsController

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -2,34 +2,42 @@ require 'spec_helper'
 
 describe Api::EventsController do
 
-  
-  describe "lists" do
+  before :each do
+    @events = (1..2).collect { mock_model(Event) }
+    Event.stub!(:order_by_video_count).and_return(@events)
+    Event.stub!(:nearby).and_return(@events)
+    Event.stub!(:paginate).and_return(@events)
+  end
 
-    before(:each) do
-      3.times do
-        Factory(:event)
+  describe 'GET index' do
+
+    context "when some events are available" do
+      it 'should get list of ALL events' do
+        params = {}
+        Event.stub!(:count => 2)
+        Event.should_receive(:count).and_return(2)
+        Event.should_receive(:paginate).with(:page => params[:page], :per_page => ITEMS_PER_PAGE).and_return(@events)
+        get :index, :format => :json
+
+        response.should be_ok
       end
-      @events = Event.all.map do |event|
-        event.api_data
+
+      it "should get list of TOP events" do
+
+      end
+
+      it "should get list of nearby events" do
+
       end
     end
 
-    it "should get list of ALL events" do
-      get :index     
-      response.code.should == '200'
+    context "when no any events are not available" do
 
-      result = JSON.parse(response.body)         
-      result.should == {"result" => @events, "success" => true, "description" => "Events list."}
     end
-    
-    it "should get list of TOP events" do
-      get :top
-      response.code.should == '200'
-      
-      result = JSON.parse(response.body)
-      result.should == {"result" => @events, "success" => true, "description" => "Top Events list."}
-    end
-    
-  end  
+
+
+
+  end
 
 end
+
