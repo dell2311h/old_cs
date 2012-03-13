01c066343fc1954ddb80da95532637843db69c95
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-13 16:13:04 +0200

    Api::SongsController ready

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,9 +1,21 @@
 class Api::SongsController < Api::BaseController
 
   def index
+    @video = Video.find params[:id]
+    @songs = @video.songs
+    
+    if @songs.count > 0
+      @songs = @songs.paginate(:page => params[:page], :per_page => ITEMS_PER_PAGE)
+      respond_with @songs, status: :ok, location: nil
+    else
+      respond_with [], status: :not_found
+    end
   end
   
   def create
+    @video = Video.find params[:id]
+    @song = @video.songs.create! params[:song]
+    respond_with @song, status: :created, location: nil
   end
   
 end
