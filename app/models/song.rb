5ea2f101ebc31fbfedb3caeac4203665e6fc9ad6
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-15 16:55:39 +0200

    Add uniqueness validation for Song name

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   has_many :video_songs, dependent: :destroy
   has_many :videos, through: :video_songs
   
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   
   scope :with_name_like, lambda {|name| where("UPPER(name) LIKE ?", "#{name.to_s.upcase}%") }
   
