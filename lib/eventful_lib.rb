2dc169be67f463e1d77e7c5f49efabd61d69a3a4
M Kurtikov <mk@mail.dimalex.eu/>
2012-03-16 14:54:37 +0200

    Eventfull event search api

--- lib/eventful_lib.rb
+++ lib/eventful_lib.rb
@@ -10,7 +10,50 @@ module EventfulLib
     def self.get_event id
       return self.eventful_api 'events/get', id
     end
-    
+
+    def self.find_events search_params
+
+      unless search_params[:latitude].nil?
+        latitude  = search_params[:latitude]
+        search_params.delete :latitude
+      end
+      unless search_params[:longitude].nil?
+        longitude = search_params[:longitude]
+        search_params.delete :longitude
+      end
+
+      unless longitude.nil? && latitude.nil?
+        search_params[:location] = "#{latitude},#{longitude}"
+        p search_params
+      end
+
+      results = self.eventful_api.call 'events/search', search_params
+      output_events = []
+      return [] if results["total_items"] < 1
+      events = results["events"]["event"]
+      
+      if results["total_items"] == 1
+        events_array = []
+        events_array.push events
+        events = events_array
+      end
+      events.each do |event|
+        tmp = {}
+        tmp[:eventful_id]     = event["id"]
+        tmp[:name]            = event["title"]
+        tmp[:date]            = event["created"]
+        tmp[:image_url] = nil
+        tmp[:image_url] = event["image"]["url"] unless event["image"].nil? || event["image"]["url"].nil?
+        output_events.push tmp
+      end
+
+      out = {}
+      out[:events] = output_events
+      out[:count] = results["total_items"]
+
+      out
+    end
+
     private
     @@eventful_api = nil
 
