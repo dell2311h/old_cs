8c1144b98d706ec52b4c70e5d35c198246685005
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-03-22 00:28:09 +0200

    Removed Video.name from list of required fields

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,19 +5,23 @@ class Video < ActiveRecord::Base
                     :s3_credentials => "#{Rails.root.to_s}/config/s3.yml",
                     :path => "/:style/:id/:filename"
 
-  validates :user_id , :event_id, :name, :presence => true
+  validates :user_id , :event_id, :presence => true
   validates :user_id, :event_id, :numericality => { :only_integer => true }
 
   validates_attachment_presence :clip
-  validates_attachment_content_type :clip, :content_type => ['video/mp4']
+  validates_attachment_content_type :clip, :content_type => ['video/mp4', 'video/quicktime']
+
+  before_create do |video|
+    video.name = video.clip_file_name if video.name.blank?
+  end
 
   belongs_to :event
   belongs_to :user
   has_many :comments, :as => :commentable, :class_name => "Comment", :dependent => :destroy
-    
+
   has_many :taggings, as: :taggable, class_name: "Tagging", dependent: :destroy
   has_many :tags, through: :taggings
-  
+
   has_many :video_songs, dependent: :destroy
   has_many :songs, through: :video_songs
 
@@ -36,4 +40,3 @@ class Video < ActiveRecord::Base
   end
 
 end
-
