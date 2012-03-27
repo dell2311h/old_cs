86282505118a8f973eba6b603118908db662df0f
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-27 16:35:21 +0300

    Add tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to :user_id }
+  it { should respond_to :commentable_id }
+  it { should respond_to :commentable_type }
+  it { should respond_to :text }
+  
+  it { should belong_to :user }
+  it { should belong_to :commentable }
+  it { should validate_presence_of :text }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :commentable_id }
+  it { should validate_presence_of :commentable_type }
 end
