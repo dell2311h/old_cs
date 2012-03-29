5663521b73dcac42fd7efa5bba837a61ba68f732
Michael Kurtikov <mk@dimalexsoftware.com>
2012-03-29 16:01:50 +0300

    Added validators for clip

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -2,4 +2,7 @@ class Clip < ActiveRecord::Base
   TYPE_DEMUX_VIDEO = 'demux_video'
   TYPE_DEMUX_AUDIO = 'demux_audio'
   belongs_to :video
+
+  validates :source , :encoding_id, :presence => true
+  validates_presence_of :video_id
 end
