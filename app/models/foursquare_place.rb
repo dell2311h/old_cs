767c87d863e0ad558cc6a02ce718e392c838ee2d
Michael Kurtikov <mk@dimalexsoftware.com>
2012-04-04 14:27:37 +0300

    Refactoring 4square api places search

---
+++ app/models/foursquare_place.rb
@@ -0,0 +1,63 @@
+class FoursquarePlace
+
+  def self.find params
+    search_params = {}
+    raise "Coordinates are not provided" unless params[:latitude] && params[:longitude]
+    check_coordinates params[:latitude], params[:longitude]
+    search_params[:ll] = "#{params[:latitude]},#{params[:longitude]}"
+    search_params[:radius] = self.cast_radius SEARCH_RADIUS
+
+    if params[:place_name]
+      search_params[:query] = params[:place_name]
+    end
+
+    raise 'Invalid  search params provided' if search_params.empty?
+
+    places =  self.foursuare_api.search_venues search_params
+
+    format_result places
+  end
+
+  private
+
+    def self.format_result input
+      output_places = []
+
+      places = input["groups"][0]["items"]
+
+      places.each do |place|
+        tmp = {}
+        tmp[:foursquare_id] = place["id"]
+        tmp[:name]          = place["name"]
+        tmp[:latitude]      = place["location"]["lat"]
+        tmp[:longitude]     = place["location"]["lng"]
+       
+        output_places.push tmp
+      end
+
+      output_places
+    end
+
+    def self.check_coordinates latitude, longitude
+      Float(latitude)
+      Float(longitude)
+    end
+
+    def self.cast_radius radius
+      radius = 1609.344 * radius
+      radius = 100000 if radius > 100000
+
+      radius
+    end
+
+    @@foursquare_api = nil
+
+    def self.foursuare_api
+      if @@foursquare_api.nil?
+         @@foursquare_api = Foursquare2::Client.new(:client_id     => Crowdsync::Application.config.foursuare_client_id,
+                                                    :client_secret => Crowdsync::Application.config.foursuare_client_secret)
+      end
+
+      @@foursquare_api
+    end
+end
