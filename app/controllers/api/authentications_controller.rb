7134803e2afc50cba6b886c6d75e7d80e92c62df
Michael Kurtikov <mk@dimalexsoftware.com>
2012-04-09 20:22:36 +0300

    Changed response codes for link/unlink profile

--- app/controllers/api/authentications_controller.rb
+++ app/controllers/api/authentications_controller.rb
@@ -2,11 +2,11 @@ class Api::AuthenticationsController < Api::BaseController
 
   def link
     @oauth = current_user.link_authentication params
-    render status: :accepted, action: :show
+    render status: :ok, action: :show
   end
 
   def destroy
     current_user.unlink_authentication params[:provider]
-    render status: :accepted, json: {}
+    render status: :ok, json: {}
   end
 end
