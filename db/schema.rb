44c99ee0ba615e221b5d35bfe56de81604daa3d2
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-10 17:55:52 +0300

    Set last_chunk_id default value to 0 at videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120409122236) do
+ActiveRecord::Schema.define(:version => 20120410140408) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -153,7 +153,7 @@ ActiveRecord::Schema.define(:version => 20120409122236) do
     t.datetime "clip_updated_at"
     t.string   "encoding_id"
     t.integer  "status",                 :default => -1
-    t.integer  "last_chunk_id"
+    t.integer  "last_chunk_id",          :default => 0
     t.string   "thumbnail_file_name"
     t.string   "thumbnail_content_type"
     t.integer  "thumbnail_file_size"
