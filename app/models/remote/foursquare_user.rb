5a133bb60a608eb40f06275a3d53ffe9b7fcba6f
Michael Kurtikov <mk@dimalexsoftware.com>
2012-04-10 20:18:21 +0300

    Implemented 4square for find local user on remote provider

--- app/models/remote/foursquare_user.rb
+++ app/models/remote/foursquare_user.rb
@@ -2,5 +2,25 @@ class Remote::FoursquareUser
   include RemoteUser
 
   def friends
+    response = api.user_friends self.uid
+
+    parse response
   end
+
+  private
+    @api
+
+    def api
+      @api = Foursquare2::Client.new({ oauth_token: self.token }) if @api.nil?
+      
+      @api
+    end
+
+    def parse response
+      response['items'].map do |element|
+        { uid: element["id"], name: (element["firstName"] +' ' + element['lastName']), avatar_url: element['photo'] }
+      end
+    end
+
+    
 end
