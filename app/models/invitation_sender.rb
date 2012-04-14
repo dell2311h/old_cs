d6bacd58c542287321064395d602279b82d39525
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-14 17:57:30 +0300

    Fix InvitationSender create and initialize methods

--- app/models/invitation_sender.rb
+++ app/models/invitation_sender.rb
@@ -1,20 +1,18 @@
 module InvitationSender
 
-  attr_reader :mode, :invitee, :code, :remote_user
+  attr_reader :invitee, :code, :remote_user
 
-  def self.create mode, invitee, code
+  def self.create mode, invitee, code, remote_user
     raise "Not implemented" unless self.class == "Module" || self.name == "InvitationSender"
     send_mode_class = @send_mode_classes[mode.to_sym]
     raise "Incorrect send mode" unless send_mode_class
 
-    send_mode_class.new mode, invitee, code, sender_uid, sender_token
+    send_mode_class.new invitee, code, remote_user
   end
 
-  def initialize(mode, invitee, code, remote_user = nil)
-    raise 'mode is empty' unless mode
+  def initialize(invitee, code, remote_user = nil)
     raise 'invitee is empty' unless invitee
     raise 'code is empty' unless code
-    @mode = mode
     @invitee = invitee
     @code = code
     @remote_user = remote_user if remote_user
@@ -29,7 +27,7 @@ module InvitationSender
   end
 
   def invitation_message
-    "<p>You you have been invited to #{Settings.application.name}</p><p>Your invitation code is #{self.code}</p><p>Follow this link to register #{self.registration_url}</p>"
+    "<p>You you have been invited to #{Settings.application.name}</p><p>Your invitation code is #{self.code}</p><p>Follow this link to register #{self.registration_url}</p>".html_safe
   end
 
   private
