2fa7f031008631f5156f7453ddaa53972486879c
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-18 20:28:46 +0300

    Add invitaion send for Twitter

--- app/models/remote/twitter_user.rb
+++ app/models/remote/twitter_user.rb
@@ -8,5 +8,16 @@ class Remote::TwitterUser
     end
   end
 
+  def post(message, link, target_uid)
+    screen_name = Twitter.user(target_uid.to_i).screen_name
+    current_user.update("@#{screen_name} #{message} #{link}")
+  end
+
+  private
+    def current_user
+      oauth_token, oauth_token_secret = self.token.split('|')
+      @twitter_user = Twitter::Client.new :consumer_key => Settings.twitter.consumer_key, :consumer_secret => Settings.twitter.consumer_secret, :oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret
+    end
+
 end
 
