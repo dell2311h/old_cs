82ae90637f3bcc63d239f70d6d3241712654e6c8
Michael Kurtikov <mk@dimalexsoftware.com>
2012-04-20 19:20:35 +0300

    Fix add_thumbnail_to_videos migration to work without paperclip

--- db/migrate/20120404142446_add_thumbnail_to_videos.rb
+++ db/migrate/20120404142446_add_thumbnail_to_videos.rb
@@ -1,11 +1,17 @@
 class AddThumbnailToVideos < ActiveRecord::Migration
   def self.up
     change_table :videos do |t|
-      t.has_attached_file :thumbnail
+    add_column :videos, :thumbnail_file_name, :string
+    add_column :videos, :thumbnail_content_type, :string
+    add_column :videos, :thumbnail_file_size, :integer
+    add_column :videos, :thumbnail_updated_at, :datetime
     end
   end
 
   def self.down
-    drop_attached_file :videos, :thumbnail
+    remove_column :videos, :thumbnail_file_name
+    remove_column :videos, :thumbnail_content_type
+    remove_column :videos, :thumbnail_file_size
+    remove_column :videos, :thumbnail_updated_at
   end
 end
