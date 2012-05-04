659ec391cbc7aacf7454be1b1e55faa049462731
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-04 21:45:59 +0300

    Add default descending ordering for comments by created_at

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
-  
+
   validates :text, :user_id, :commentable_id, :commentable_type, :presence => true
 
   self.per_page = Settings.paggination.per_page
+
+  default_scope order("created_at DESC")
+
 end
+
