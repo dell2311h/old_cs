1b74041a0830487af8e24c4a7211f1d084780b96
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-07 11:42:14 +0300

    Add :event_id to Song.with_videos_count

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   scope :with_name_like, lambda {|name| where("UPPER(name) LIKE ?", "%#{name.to_s.upcase}%") }
 
-  scope :with_videos_count, lambda { |params = {}| select("songs.*").select("(#{VideoSong.select("COUNT(video_songs.song_id)").where("songs.id = video_songs.song_id").to_sql}) AS videos_count")
+  scope :with_videos_count, lambda { |params = {}| select("songs.*").select("(#{VideoSong.select("COUNT(video_songs.song_id)").where("songs.id = video_songs.song_id").joins(:video).where("(videos.event_id = ?) OR (? IS NULL)", params[:event_id], params[:event_id]).to_sql}) AS videos_count")
   }
 
   scope :with_comments_count, lambda { |params = {}| select("songs.*").select("SUM((#{Comment.select("COUNT(comments.commentable_id)").where("videos.id = comments.commentable_id AND comments.commentable_type = 'Video'").where("(videos.event_id = ?) OR (? IS NULL)", params[:event_id], params[:event_id]).to_sql})) AS comments_count").joins(:videos).group("songs.id")
