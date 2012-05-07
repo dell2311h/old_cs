d9981c5d15a64fdfb4a330e2c55ffd6cd8002f02
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-07 11:29:49 +0300

    Add search paramter event_id to Song.with_calculated_counters scope

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,11 +8,13 @@ class Song < ActiveRecord::Base
 
   scope :with_name_like, lambda {|name| where("UPPER(name) LIKE ?", "%#{name.to_s.upcase}%") }
 
-  scope :with_videos_count, select("songs.*").select("(#{VideoSong.select("COUNT(video_songs.song_id)").where("songs.id = video_songs.song_id").to_sql}) AS videos_count")
+  scope :with_videos_count, lambda { |params = {}| select("songs.*").select("(#{VideoSong.select("COUNT(video_songs.song_id)").where("songs.id = video_songs.song_id").to_sql}) AS videos_count")
+  }
 
-  scope :with_comments_count, select("songs.*").select("SUM((#{Comment.select("COUNT(comments.commentable_id)").where("videos.id = comments.commentable_id AND comments.commentable_type = 'Video'").to_sql})) as comments_count").joins(:videos).group("songs.id")
+  scope :with_comments_count, lambda { |params = {}| select("songs.*").select("SUM((#{Comment.select("COUNT(comments.commentable_id)").where("videos.id = comments.commentable_id AND comments.commentable_type = 'Video'").where("(videos.event_id = ?) OR (? IS NULL)", params[:event_id], params[:event_id]).to_sql})) AS comments_count").joins(:videos).group("songs.id")
+  }
 
-  scope :with_calculated_counters, with_videos_count.with_comments_count
+  scope :with_calculated_counters, lambda { |params = {}| with_videos_count(params).with_comments_count(params) }
 
   scope :search, lambda { |params|
 
