fb24ce1c4e7133d8c3b3a7504682beadfcc70094
Michael Kurtikov <mk@dimalexsoftware.com>
2012-05-07 20:41:15 +0300

    Fix for event playlist creation

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -14,15 +14,23 @@ class Playlist
     track_to_add = 0
     track_place_to_add = 0
     begin
-      @timelines.each do |track|
+      @timelines.each_with_index do |track, track_number|
         track_videos = track[:clips]
-        track_videos.each do |track_video|
-          throw 'Break loops' if should_add? video, track_video
-          track_place_to_add = track_place_to_add + 1
-        end
-        track_place_to_add = 0
-        track_to_add = track_to_add + 1
+        track_videos.each_with_index do |track_video, video_number|
 
+          is_first_element = false
+          next_track_video = nil
+          is_first_element = true if video_number == 0
+          next_track_video = track_videos[video_number+1] unless video_number == track_videos.size - 1
+
+           result = should_add(video, track_video, next_track_video, is_first_element)
+            unless result == false
+            track_place_to_add = video_number + result
+            throw "Break loops"
+          end
+        end
+        track_to_add = track_number + 1
+        track_place_to_add = 0
       end
     rescue
     ensure
@@ -30,11 +38,12 @@ class Playlist
     end
   end
 
-  def should_add? video, track_video
-    return true if video.end_time   <= track_video.start_time
-    return true if video.start_time >= track_video.end_time
+  def should_add video, track_video, next_track_video, is_first_element
+    return Playlist::ADD_BEFORE if is_first_element && track_video.start_time >= video.end_time
+    return Playlist::ADD_NEXT  if next_track_video.nil? && track_video.end_time <= video.start_time
+    return Playlist::ADD_NEXT  if track_video.end_time <= video.start_time && next_track_video.start_time >= video.end_time
 
-    false
+    Playlist::DO_NOT_ADD
   end
 
   def add_to_track video, track = 0, place = 0
@@ -48,5 +57,10 @@ class Playlist
     @timelines[track][:clips].insert place - 1, video
     @timelines[track][:size] = @timelines[track][:size] + 1
   end
+
+  private
+    DO_NOT_ADD = false
+    ADD_NEXT = 2
+    ADD_BEFORE = 1
 
 end
