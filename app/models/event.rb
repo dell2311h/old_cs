61f1e9c3f7671844b3521ebeb7bf8a702ed95119
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-10 16:50:53 +0300

    Timings creation ready. Testing needed

--- app/models/event.rb
+++ app/models/event.rb
@@ -57,6 +57,45 @@ class Event < ActiveRecord::Base
     playlist.timelines
   end
 
+  # Create mastetrack placeholder
+  def create_not_ready_master_track
+    last_master_track = self.master_tracks.first
+    self.master_tracks.create :version => (last_master_track ? (last_master_track.version + 1) : 0)
+  end
+
+  # Timings creation
+  def create_timings_by_pluraleyes_sync pe_sync_results = []
+    # Prepare new master track
+    new_master_track = self.create_not_ready_master_track
+
+    # prepare PluralEyes results for timings creation
+    groups_with_timestamp_and_duration = []
+    pe_sync_results.each do |group|
+      sorted_group = group.sort { |x, y| x[:start].to_i <=> y[:start].to_i }
+      first_synched_clip = sorted_group[0]
+      clip = Clip.find_by_pluraleyes_id first_synched_clip[:media_id]
+      timestamp = clip.video.meta_info.recorded_at.to_i
+      last_synched_clip = sorted_group.last
+      group_duration = last_synched_clip[:end]
+      groups_with_timestamp_and_duration << { pe_group: sorted_group, starts_at: timestamp, duration: group_duration }
+    end
+
+    # Sort groups by their chronological order
+    groups_with_timestamp_and_duration.sort! { |x, y| x[:starts_at] <=> y[:starts_at] }
+
+    # create timings by prepared PluralEyes results
+    group_time_offset = 0 # miliseconds
+    groups_with_timestamp_and_duration.each do |group|
+      # group { :pe_group => [...], :starts_at => timestamp, :duration => miliseconds }
+      pluraleyes_group = group[:pe_group]
+      pluraleyes_group.each do |synced_clip|
+        clip = Clip.find_by_pluraleyes_id synced_clip[:media_id]
+        timing = clip.video.timings.create! :start_time => synced_clip[:start], :end_time => synced_clip[:end], :version => new_master_track.version
+      end
+      group_time_offset += group[:duration]
+    end
+  end
+
 
   private
 
--- app/models/master_track.rb
+++ app/models/master_track.rb
@@ -1,7 +1,10 @@
 class MasterTrack < ActiveRecord::Base
   belongs_to :event
 
-  validates :event_id, :source, :version, presence: true
+  validates :event_id, :version, presence: true
+  validates :source, presence: true, :if => "is_ready == true"
+
+  default_scope order("version DESC")
 
 end
 
