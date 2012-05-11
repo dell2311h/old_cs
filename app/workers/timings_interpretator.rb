33aa0d11e50c3620745f460a755e01905d3fe59b
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-11 16:34:07 +0300

    Add TimingsInterpetator Resque worker

---
+++ app/workers/timings_interpretator.rb
@@ -0,0 +1,14 @@
+class TimingsInterpetator
+
+  @queue = :timings_queue
+
+  def self.perform(event_id)
+    event = Event.find event_id
+    event.sync_with_pluraleyes
+
+    # TODO: send master track creation request to Encoding Server
+
+  end
+
+end
+
