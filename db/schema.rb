337edcdf6da830b52f48b8927a59b0b3f4eaae49
Michael Kurtikov <mk@dimalexsoftware.com>
2012-05-15 19:38:19 +0300

    Added encoding_profile model to store encoding profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508182224) do
+ActiveRecord::Schema.define(:version => 20120515160553) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(:version => 20120508182224) do
     t.text     "text"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+  end
+
+  create_table "encoding_profiles", :force => true do |t|
+    t.string   "profile_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "events", :force => true do |t|
---
+++ spec/factories/encoding_profiles.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :encoding_profile do
+    profile_id "MyString"
+    name "MyString"
+  end
+end
