19468017e0f9a06a090bcc00e21c83b255ec924c
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-05-17 18:43:04 +0300

    Refactored demux handler

--- app/models/encoding_handler/demux.rb
+++ app/models/encoding_handler/demux.rb
@@ -2,20 +2,25 @@ class EncodingHandler::Demux
 
   def perform params
     video = find_video params
-    medias = get_medias params
-    update_clips video.id, Clip::TYPE_DEMUX_AUDIO, medias[:audio]
-    clip = update_clips video.id, Clip::TYPE_DEMUX_VIDEO, medias[:video]
-
-    send_to_streaming clip, video
+    clips = update_clips video.id, params[:medias]
+    send_to_streaming clips["demuxed_video"], video
   end
 
   private
 
     def send_to_streaming clip, video
+      profile = EncodingProfile.find_by_name "streaming"
       params = { :profile_id => profile.profile_id,
-                 :encoder => { :input_media_ids => clip.encoding_id,
-                               :params => { :media_id => clip.encoding_id,
-                                            :destination => "encoded/#{video.event_id}/#{video.id}/streaming"
+                 :encoder => { :input_media_ids => [clip.encoding_id],
+                               :params => { :destination_1 => "encoded/#{video.event_id}/#{video.id}/streaming/low/160x240.mp4",
+                                            :destination_2 => "encoded/#{video.event_id}/#{video.id}/streaming/normal/160x240.mp4",
+                                            :destination_3 => "encoded/#{video.event_id}/#{video.id}/streaming/high/160x240.mp4",
+                                            :destination_4 => "encoded/#{video.event_id}/#{video.id}/streaming/low/320x480.mp4",
+                                            :destination_5 => "encoded/#{video.event_id}/#{video.id}/streaming/normal/320x480.mp4",
+                                            :destination_6 => "encoded/#{video.event_id}/#{video.id}/streaming/high/320x480.mp4",
+                                            :destination_7 => "encoded/#{video.event_id}/#{video.id}/streaming/low/640x960.mp4",
+                                            :destination_8 => "encoded/#{video.event_id}/#{video.id}/streaming/normal/640x960.mp4",
+                                            :destination_9 => "encoded/#{video.event_id}/#{video.id}/streaming/high/640x960.mp4"
                                           }
                              }
               }
@@ -23,40 +28,23 @@ class EncodingHandler::Demux
       raise 'Unable to add video to demux' unless status
     end
 
-    def update_clips video_id, type, media
-      clip = Clip.find_or_initialize_by_video_id_and_clip_type(video_id, type)
-      clip.update_attributes({ :source      => media[:source],
-                               :encoding_id => media[:encoding_id],
-                               :clip_type   => type,
-                            })
-
-      raise 'Unable to save clip errors: ' + clip.errors.to_json unless clip.errors.empty?
-      clip.save
-      Rails.logger.info "Created demux clip(encoding_id #{media[:encoding_id]}) for video id# #{video_id.to_s}"
-
-      clip
-    end
-
-    def get_medias params
-      audio = nil
-      video = nil
-
-      params[:medias].each do |media|
-        tmp = {}
-        tmp[:source] = media["location"]
-        tmp[:encoding_id] = media["_id"]
-        audio = tmp if media["type"] == 'audio'
-        video = tmp if media["type"] == 'video'
+    def update_clips video_id, medias
+      clips = {}
+      medias.each do |media|
+        clip = Clip.find_or_initialize_by_video_id_and_clip_type(video_id, media[:type])
+        clip.source      = media["location"]
+        clip.encoding_id = media["_id"]
+        clip.clip_type   = media["type"]
+        clip.save
+        raise 'Unable to save clip errors: ' + clip.errors.to_json unless clip.errors.empty?
+        clips[media["type"]] = clip
+        Rails.logger.info "Created demux clip(encoding_id #{media[:encoding_id]}) for video id# #{video_id.to_s}"
       end
-
-      raise 'Unable to get medias' if audio.nil? || video.nil?
-
-      { :audio =>audio, :video => video }
+      clips
     end
 
     def find_video params
       raise 'Wrong params' if params[:input_media_ids].nil? || params[:input_media_ids].first.nil?
-
       Video.unscoped.find_by_encoding_id! params[:input_media_ids].first
     end
 
