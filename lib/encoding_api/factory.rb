809ff6e23a8e92edf506f1787c1febf3fb7e7a2b
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-17 17:12:09 +0300

    CAT

--- lib/encoding_api/factory.rb
+++ lib/encoding_api/factory.rb
@@ -1,11 +1,13 @@
 require "encoding_api/profile"
 require "encoding_api/demux"
 require "encoding_api/add_media"
+require "encoding_api/master_track"
+
 module EncodingApi
   @logger = Logger.new(Rails.root.to_s + '/log/encoding_' + Rails.env + '.log')
 
   def self.log text
-      @logger.info Time::now.to_s + "\n" + text + "\n"
+    @logger.info Time::now.to_s + "\n" + text + "\n"
   end
 
   class Factory
@@ -19,3 +21,4 @@ module EncodingApi
   end
 
 end
+
