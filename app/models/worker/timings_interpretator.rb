947a5f8f5ed357cdde3a259b99d82275041ddebb
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-17 17:49:26 +0300

    Move Event#make_new_master_track method call to TimingsInterpretator

--- app/models/worker/timings_interpretator.rb
+++ app/models/worker/timings_interpretator.rb
@@ -4,10 +4,7 @@ class Worker::TimingsInterpretator
 
   def self.perform(event_id)
     event = Event.find event_id
-    event.sync_with_pluraleyes
-
-    # TODO: send master track creation request to Encoding Server
-
+    event.make_new_master_track
   end
 
 end
