6ccde09b81a1b0e68562275be423d9da548d284f
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-22 18:56:36 +0300

    Add Thumbnails profile handler. Add send demuxed video to thumbnails creation

--- app/models/encoding_handler/demux.rb
+++ app/models/encoding_handler/demux.rb
@@ -3,6 +3,7 @@ class EncodingHandler::Demux < EncodingHandler::Base
   def perform params
     video = find_video params
     clips = update_clips video.id, params[:medias]
+    send_to_thumbnalization clips["demuxed_video"], video
     send_to_streaming clips["demuxed_video"], video
   end
 
@@ -28,9 +29,21 @@ class EncodingHandler::Demux < EncodingHandler::Base
       raise 'Failed to send video to streaming' unless response["status"] == 'ok'
     end
 
+    def send_to_thumbnalization clip, video
+      profile = EncodingProfile.find_by_name "thumbnails"
+      params = { :profile_id => profile.profile_id,
+                 :encoder => { :input_media_ids => [clip.encoding_id],
+                               :params => { :destination => "encoded/#{video.event_id}/#{video.id}/thumbnails" }
+                             }
+               }
+      response = Pandrino::Api.deliver Settings.encoding.url.actions.encoders, params
+      raise 'Failed to send video to making thumbnails' unless response["status"] == 'ok'
+    end
+
     def find_video params
       raise 'Wrong params' if params[:input_media_ids].nil? || params[:input_media_ids].first.nil?
       Video.unscoped.find_by_encoding_id! params[:input_media_ids].first
     end
 
 end
+
