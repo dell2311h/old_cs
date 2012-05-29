f51be7def8de1f94f0579f700c10e1acd896cb84
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-29 18:01:00 +0300

    Add system type for rvm at deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,8 @@ set :application, "crowdsync" # Name of application
 set :repository,  "git@github.com:111minutes/cs-server.git"
 
 set :rvm_ruby_string, "1.9.2-p290@#{application}"
+set :rvm_type, :system
+
 set :default_stage, "staging"
 set :stages, %w(staging)
 
