01bfc16f732dfe3d6beaa4c342d0fa58d966d8dd
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-30 12:05:57 +0300

    Fix deploy script. Set precompilation of assets at each deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -56,13 +56,8 @@ namespace :deploy do
 
   desc "Precompile assets"
   namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
+    task :precompile, :roles => :app, :except => { :no_release => true } do
+      run "cd #{latest_release} && RAILS_ENV=#{rails_env} rake assets:precompile"
     end
   end
 
