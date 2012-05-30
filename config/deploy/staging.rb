536acbd56d8b85906cd3381ca7091a5d62543640
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-30 20:06:17 +0300

    Fix videos index. Fix server domain name at deploy script (temporary solution)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -16,7 +16,9 @@ ssh_options[:forward_agent] = true
 set :use_sudo, false
 
 # Deployment server
-server "#{application}.dimalexsoftware.com", :app, :web, :db, :primary => true
+#server "#{application}.dimalexsoftware.com", :app, :web, :db, :primary => true
+server "ec2-50-19-12-203.compute-1.amazonaws.com", :app, :web, :db, :primary => true # temporary solution
+
 
 # Rails environment
 set :rails_env, "production"
