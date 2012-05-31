7077a6b27594c060d2d58a32cd5005ddf9ccb4a9
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-05-31 13:06:51 +0300

    Fix tests env error

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,11 +5,42 @@ describe Comment do
   it { should respond_to :commentable_id }
   it { should respond_to :commentable_type }
   it { should respond_to :text }
-  
+
   it { should belong_to :user }
   it { should belong_to :commentable }
   it { should validate_presence_of :text }
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :commentable_id }
   it { should validate_presence_of :commentable_type }
+
+  describe "#commentable_unscoped" do
+
+    let(:hidden_video) { Factory.create :video, :status => Video::STATUS_UPLOADING }
+    let(:comment) { Factory.create :comment, :commentable => hidden_video }
+
+    it "should find unscoped commentable" do
+      comment.commentable_unscoped.should == hidden_video
+    end
+
+  end
+
+  describe "#destroy_by(user)" do
+
+    it "should delete comment by its owner" do
+
+    end
+
+    it "should delete comment by owner of commented video" do
+
+    end
+
+    it "should not delete comments by other users" do
+
+    end
+
+  end
+
+
+
 end
+
