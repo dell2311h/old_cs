a0f6ae0adfc9e4f30f0e78e92418163ef877b1fd
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-06-05 12:39:07 +0200

    Change storage for clip_uploader to local mounted S3 bucket

--- app/uploaders/clip_uploader.rb
+++ app/uploaders/clip_uploader.rb
@@ -1,18 +1,12 @@
 # encoding: utf-8
 
 class ClipUploader < CarrierWave::Uploader::Base
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  storage :file
 
   self.remove_previously_stored_files_after_update = false
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore.pluralize}/#{model.id}/#{mounted_as}/"
+    "#{Settings.carrierwave.base_url}uploads/#{model.class.to_s.underscore.pluralize}/#{model.id}/#{mounted_as}/"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -20,4 +14,3 @@ class ClipUploader < CarrierWave::Uploader::Base
     %w(m4v mp4)
   end
 end
-
