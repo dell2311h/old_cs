24358591b775432c09a2751a54253a702ca1dc26
Michael Kurtikov <michael.kurtikov@gmail.com>
2012-06-07 19:40:55 +0300

    Index profiles doesn't require authentification

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,12 +1,11 @@
 class Api::ProfilesController < Api::BaseController
 
+  skip_before_filter :auth_check, :only => [:index]
+
   def index
     raise I18n.t 'errors.parameters.empty_name' if params[:name].nil?
-    @performer = Performer.search({ :performer_name => params[:name] })
-    @user = User.search_by({ :name => params[:name] }, current_user)
-    
-    @performer = @performer.first
-    @user = @user.first
+    @performer = Performer.search({ :performer_name => params[:name] }).first
+    @user = User.with_name_like(params[:name]).first
 
     render :status => :not_found, json: {} if @user.nil? && @performer.nil?
   end
