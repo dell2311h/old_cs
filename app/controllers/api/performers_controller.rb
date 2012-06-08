a9df5a673f5896963f698fbcf83e76deb11440fe
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-06-08 18:02:44 +0300

    Add flags and counters calculation to Events Places and Perfrormers controllers. Fix Follow::FlagsAndCounters module

--- app/controllers/api/performers_controller.rb
+++ app/controllers/api/performers_controller.rb
@@ -13,14 +13,14 @@ class Api::PerformersController < Api::BaseController
   def index
     @performers = Performer.search params
     if @performers.count > 0
-      @performers = @performers.paginate(:page => params[:page], :per_page => params[:per_page])
+      @performers = @performers.paginate(:page => params[:page], :per_page => params[:per_page]).with_flag_followed_by(current_user).with_calculated_counters
     else
       render :status => :not_found, json: {}
     end
   end
 
   def show
-    @performer = Performer.find params[:id]
+    @performer = Performer.with_flag_followed_by(current_user).with_calculated_counters.find params[:id]
   end
 
   def create
--- app/models/follow/flags_and_counters.rb
+++ app/models/follow/flags_and_counters.rb
@@ -1,9 +1,11 @@
 module Follow::FlagsAndCounters
   def self.included(base)
       base.scope :with_flag_followed_by, lambda { |user|
-      model_name = base.scoped.klass.to_s
-      table_name = base.scoped.table.name
-      base.select("#{table_name}.*").select("(#{Relationship.select('COUNT(follower_id)').where("relationships.followable_type = '#{model_name}' AND relationships.followable_id = #{table_name}.id AND relationships.follower_id = ?", user.id).to_sql}) AS followed")
+      if user
+        model_name = base.scoped.klass.to_s
+        table_name = base.scoped.table.name
+        base.select("#{table_name}.*").select("(#{Relationship.select('COUNT(follower_id)').where("relationships.followable_type = '#{model_name}' AND relationships.followable_id = #{table_name}.id AND relationships.follower_id = ?", user.id).to_sql}) AS followed")
+      end
     }
 
     base.scope :with_followers_count, lambda {
