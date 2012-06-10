230e83d5cefd9d1049e43d78ff885e807781e711
Michael Kurtikov <michael.kurtikov@gmail.com>
2012-06-10 15:56:34 +0300

    Spec for comments controller

--- spec/controllers/api/comments_controller_spec.rb
+++ spec/controllers/api/comments_controller_spec.rb
@@ -5,7 +5,7 @@ describe Api::CommentsController do
   before :each do
     @result_array = []
     @video = Video
-    @video.stub!(:find_video_by).and_return(@video)
+    @video.stub!(:find_by).and_return(@video)
     @video.stub!(:comments).and_return(@result_array)
   end
 
@@ -66,7 +66,7 @@ describe Api::CommentsController do
     end
 
     context "when no results avaliable" do
-      it "should tender not found status" do
+      it "should render not found status" do
         
         @result_array.stub!(:count).and_return(0)
         @event.should_receive(:find).with(@event_id).and_return(@event)
@@ -82,12 +82,14 @@ describe Api::CommentsController do
     before :each do
       @video = Factory.create :video
       @user = Factory.create :user
+      Video.unstub!(:find_by)
+      Video.unstub!(:comments)
     end
     context "when creation successfull" do
       it "should create comment and return ok status" do
         lambda{ post :create, :format => :json,
                      :authentication_token=> @user.authentication_token,
-                     :comment => { :text => "!!", :user_id => user.id },
+                     :comment => { :text => "!!", :user_id => @user.id },
                      :id => @video.id
               }.should change(@video.comments, :count).by(1)
         response.should be_ok
@@ -106,7 +108,29 @@ describe Api::CommentsController do
   end
 
   describe "DELETE delete" do
-    
+    before :all do
+      @comment = Factory.create :comment
+    end
+    context "when uset can delete comment" do
+      it "should render ok status" do
+        delete :destroy, :format => :json,
+                         :authentication_token=> @comment.user.authentication_token,
+                         :comment_id =>@comment.id
+        Comment.find_by_id(@comment.id).should be_nil
+        should respond_with :accepted
+      end
+    end
+
+    context "when user can't delete comment" do
+      user = Factory.create :user
+      it "should render bad request status" do
+        delete :destroy, :format => :json,
+                         :authentication_token=> user.authentication_token,
+                         :comment_id =>@comment.id
+        Comment.find_by_id(@comment.id).should_not be_nil
+        response.should be_bad_request
+      end
+    end
   end
 
 end
