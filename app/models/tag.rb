8e613c3817a11c69a3e9755424b893738b06c44e
Michael Kurtikov <michael.kurtikov@gmail.com>
2012-06-12 18:13:36 +0300

    Tags creation after comments is saved

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,14 +6,23 @@ class Tag < ActiveRecord::Base
 
   validates :name, presence: true
 
-  def self.add_for(taggable, tags)
+  def self.create_by_comment comment
+    tags = find_tags_in comment.text
     tags.each do |tag_name|
       tag = Tag.find_or_create_by_name(tag_name.downcase)
-      taggable.tags << tag if !taggable.tags.find_by_id(tag)
+      tagging = tag.taggings.build({ :user_id    => comment.user_id,
+                                     :video_id   => comment.video_id,
+                                     :comment_id => comment.id })
+      tagging.save
     end
-    taggable.tags.map(&:name)
+
   end
 
+  private 
+
+  def self.find_tags_in comment_text
+    comment_text.scan(/#(\S*)(?:\z|\s)/).uniq.flatten
+  end
 
 end
 
