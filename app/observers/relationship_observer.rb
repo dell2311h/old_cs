dab40ce89e79001496924f8c47f8ab63ba8a15e1
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-06-14 19:46:45 +0300

    Add RelationshipObserver

--- app/observers/relationship_observer.rb
+++ app/observers/relationship_observer.rb
@@ -1,2 +1,8 @@
 class RelationshipObserver < ActiveRecord::Observer
+
+  def after_create(model)
+    FeedItem.create!(:action => 'follow', :user => model.follower, :itemable => model.followable)
+  end
+
 end
+
