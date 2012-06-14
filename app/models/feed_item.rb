8f5659b927cbadcfd2a0f7bf1e955d82b96f0fc1
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-06-14 19:48:01 +0300

    Add feed message methods to FeedItem

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -1,6 +1,6 @@
 class FeedItem < ActiveRecord::Base
 
-  ALLOWED_ITEMABLES = ["User", "Video", "Song", "Comment"]
+  ALLOWED_ITEMABLES = ["User", "Video", "Song", "Comment", "Event", "Place", "Performer"]
   ALLOWED_CONTEXTABLES = ["Video", "Event", "Comment", "Authentication"]
   ALLOWED_ACTIONS = ["video_upload", "comment_video", "follow", "mention", "like_video", "join_crowdsync", "add_song"]
 
@@ -14,6 +14,34 @@ class FeedItem < ActiveRecord::Base
   validates :itemable_type, :inclusion => ALLOWED_ITEMABLES
   validates :action, :inclusion => ALLOWED_ACTIONS
   validates :contextable_type, :inclusion => ALLOWED_CONTEXTABLES, :if => lambda { |f| f.contextable_type }
+
+  def message_for_feed(feed_type)
+    raise "Not allowed feed type" unless [:user, :news, :notification].include?(feed_type)
+    I18n.t "feed.#{feed_type}.#{self.action}"
+  end
+
+  def item_name
+    case self.itemable_type
+      when "User"
+        self.itemable.username
+      when /Place|Performer|Event|Song/
+        self.itemable.name
+      else
+        ""
+    end
+  end
+
+  def actor
+    { :type => 'User', :id => self.user_id }
+  end
+
+  def item
+    { :type => self.itemable_type, :id => self.itemable_id }
+  end
+
+  def context
+    { :type => self.contextable_type, :id => self.contextable_id }
+  end
 
 end
 
