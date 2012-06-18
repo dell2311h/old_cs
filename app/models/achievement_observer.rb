417a71d5b63f5ab63d3747c25cea60a452b0ec5c
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-06-18 12:12:21 +0300

    Added receiving achievement points when user uploads first video to event

--- app/models/achievement_observer.rb
+++ app/models/achievement_observer.rb
@@ -3,7 +3,13 @@ class AchievementObserver < ActiveRecord::Observer
 
   def after_upload(video)
     AchievementPoint.create(:user_id => video.user_id,
-                             :reason_code => AchievementPoint::REASONS[:upload_video],
-                             :points => Settings.achievements.points.upload_video)
+                            :reason_code => AchievementPoint::REASONS[:upload_video],
+                            :points => Settings.achievements.points.upload_video)
+    first_video = Video.unscoped.where(:event_id => video.event_id).order("created_at ASC").first
+    if video.id == first_video.id
+      AchievementPoint.create(:user_id => video.user_id,
+                              :reason_code => AchievementPoint::REASONS[:first_upload_to_event],
+                              :points => Settings.achievements.points.first_upload_to_event)
+    end
   end
 end
