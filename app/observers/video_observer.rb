d0770853e770e573020d23027580ba52dc22b323
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-06-19 17:57:43 +0300

    Fix Comment and Video observers

--- app/observers/video_observer.rb
+++ app/observers/video_observer.rb
@@ -3,7 +3,9 @@ class VideoObserver < ActiveRecord::Observer
   def after_set_status_done(video)
     FeedItem.create(:action      => "video_upload",
                     :user_id     => video.user_id,
-                    :entity      => video)
+                    :entity      => video,
+                    :context => video.event)
   end
 
 end
+
