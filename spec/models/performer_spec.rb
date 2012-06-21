28c45a70e52bee6d9c79bb1d87faa820b8a68a24
Michael Kurtikov <michael.kurtikov@gmail.com>
2012-06-21 14:33:03 +0300

    Fix performers_spec

--- spec/models/performer_spec.rb
+++ spec/models/performer_spec.rb
@@ -7,8 +7,6 @@ describe Performer do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
 
-  it { should have_and_belong_to_many(:events) }
-
   describe "#search" do
     before :all do
       2.times { Factory.create :performer }
