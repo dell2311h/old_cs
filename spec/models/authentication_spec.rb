58f35b429ca089614e4f4e3223219cc1af89a549
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2012-06-27 17:34:25 +0300

    Fixed broken tests

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe Authentication do
 
+  before :each do
+    FeedItem.stub!(:create_for_join_crowdsync_via_social_network)
+  end
+
+  let(:authentication) { Factory(:authentication) }
+  subject { authentication }
+
   it { should respond_to :user_id }
   it { should respond_to :provider }
   it { should respond_to :uid }
