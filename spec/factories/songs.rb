aa3feec14d0b46aa34b70095ed1b19d9337e779f
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-04 18:36:27 +0300

    Skip callbacks at Song factory

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,10 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :song do |f|
+    f.after_build { |song| song.class.skip_callback(:create, :after, :accure_achievement_points) }
     f.name { "#{Faker::Lorem.word.capitalize}_#{(1..99999).to_a.sample}" }
     f.association :user
-    f.after_create {|song| Factory(:video_song, :song => song) }
   end
+
 end
 
