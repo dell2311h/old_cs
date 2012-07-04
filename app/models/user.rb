a407835e2556c53007d3b510c4e405624f107c7b
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-04 14:06:30 +0300

    Fix User#unlink_authentication(provider) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -177,7 +177,8 @@ class User < ActiveRecord::Base
   end
 
   def unlink_authentication provider
-    Authentication.delete_all([ "provider = ? AND user_id = ?",provider, self.id ])
+    authentication = self.authentications.find_by_provider(provider)
+    authentication.destroy if authentication
   end
 
   def update_coordinates coordinates
