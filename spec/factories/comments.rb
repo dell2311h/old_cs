bd17d1d8ff17a80be977616e16d88371e0583bf5
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-04 18:35:45 +0300

    Add without callbacks factory for Comment

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,9 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :comment do |f|
     f.association :user
     f.association :video
     f.text { Faker::Lorem.sentence }
   end
+
+  factory :comment_without_callbacks, :parent => :comment do |f|
+    f.after_build { |comment| comment.class.skip_callback(:create, :after, :accure_achievement_points) }
+    f.after_build { |comment| comment.class.skip_callback(:create, :after, :create_tags) }
+  end
+
 end
+
