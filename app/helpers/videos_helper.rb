85577d7be3af296d6bc1ca48d130f45a3e5f90ed
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-06 12:44:48 +0300

    Add textual_status_for(video) helper method

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,13 @@
 module VideosHelper
+  def textual_status_for(video)
+    textual_statuses = {
+      Video::STATUS_UPLOADING => I18n.t('descriptions.video.status.uploading'),
+      Video::STATUS_NEW => I18n.t('descriptions.video.status.new'),
+      Video::STATUS_IN_PROCESSING => I18n.t('descriptions.video.status.processing'),
+      Video::STATUS_PROCESSING_DONE => I18n.t('descriptions.video.status.processing_done')
+    }
+
+    textual_statuses[video.status]
+  end
 end
+
