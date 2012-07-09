c894726b4ef0f0f36bd89bc7ec0f8fd96f7b5204
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-09 13:11:59 +0300

    31588339 Fix validation at Song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,8 @@ class Song < ActiveRecord::Base
 
   after_create :accure_achievement_points
 
-  validates :name, presence: true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true
+  validates :user_id, :presence => true
 
   scope :suggestions_by_name, lambda {|name| where("UPPER(name) LIKE ?", "#{name.to_s.upcase}%") }
 
