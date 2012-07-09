c38ad14e03895ad34c60792ed332647971b87b21
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-07-09 13:28:02 +0300

    31588339 Add human readable messages for VideoPerformer model

--- app/models/video_performer.rb
+++ app/models/video_performer.rb
@@ -3,7 +3,7 @@ class VideoPerformer < ActiveRecord::Base
   belongs_to :performer
 
   validates :performer_id, :video_id, :presence => true
-  validates :performer_id, :uniqueness => { :scope => :video_id, :message => I18n.t('errors.models.video_performer.uniqueness_by_performer') }
+  validates :performer_id, :uniqueness => { :scope => :video_id }
 
 end
 
