4fe4f541659b0709160fc991d4bc0a483970b485
Michael Kurtikov <michael.kurtikov@gmail.com>
2012-07-12 14:30:07 +0300

    #32547907 - Fix device unregister mechanism

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,15 +3,16 @@ class Device < ActiveRecord::Base
   validates_format_of :token, :with => /^[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}$/, :allow_nil => true
 
   def self.register_device(user, token)
-    Device.unregister_device user
+    Device.unregister_device user, token
     device = Device.create({:user => user, :token => token})
     device.save!
 
     device
   end
 
-  def self.unregister_device(user)
+  def self.unregister_device(user, token = nil)
     user.device.destroy if user.device
+    Device.destroy_all(:token => token) if token
   end
 
 end
